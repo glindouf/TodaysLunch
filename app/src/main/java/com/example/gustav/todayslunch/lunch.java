e952f361d2a20c31a484e42879e3aaabaf726e65
ludvi <ludvig.sorfolk@gmail.com>
2017-11-14 14:51:43 +0100

    new changes

--- app/src/main/java/com/example/gustav/todayslunch/lunch.java
+++ app/src/main/java/com/example/gustav/todayslunch/lunch.java
@@ -8,15 +8,21 @@ import android.widget.ArrayAdapter;
 
 public class lunch extends AppCompatActivity {
     ListView menu;
-    String[] menuItems = {"Crabonara","Pannbiff","Diavola"};
+    String[] menuItems = {"Monday","Crabonara","Pannbiff","Diavola", "Butterfingers",
+            "Tuesday", "Grillad fläskkarré","Husetsfiskgratäng", "Stektlax"};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch);
-        TextView t=(TextView)findViewById(R.id.title);
-        t.setText("Mimolett");
+        TextView title=(TextView)findViewById(R.id.title);
+        title.setText("Mimolett");
+        TextView info =(TextView)findViewById(R.id.info);
+        info.setText("Lindholmsallén 61         031-224466");
+
+
+
         menu = (ListView) findViewById(R.id.menu);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,menuItems);
         menu.setAdapter(arrayAdapter);
